94e70ef695abbc1551069b3e2ec212c0abfbc9a3
Israel <israel.usp@gmail.com>
2021-04-26 22:34:49 -0300

    Aula 2.03
    - Criados modelos para Alunos, Cursos, AdminAlunos e AdminCursos;

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+
+class ListarAlunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'dataNascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = (['nome'])
+    list_per_page = 20
+
+admin.site.register(Aluno, ListarAlunos)
+
+
+class ListarCursos(admin.ModelAdmin):
+    list_display = ('id', 'cdCurso')
+    list_display_links = ('id', 'cdCurso')
+    search_fields = (['cdCurso'])
+
+admin.site.register(Curso, ListarCursos)
--- escola/models.py
+++ escola/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=60)
-    rg = models.CharField(max_length=8)
+    rg = models.CharField(max_length=9)
     cpf = models.CharField(max_length=11)
     dataNascimento = models.DateField()
 
