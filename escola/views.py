a144a7d22c1b2ddc3ad643f567a49de69780d2f4
Israel <israel.usp@gmail.com>
2021-04-28 22:54:42 -0300

    Aula5.03
    - Criando outro tipo de apresentação de "aggregates"

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializers import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculaAlunoSerializer
+from escola.serializers import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculaAlunoSerializer, ListaAlunosMatriculadosSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -23,3 +23,10 @@ class ListaMatriculasAluno(generics.ListAPIView):
         queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculaAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Exibindo os alunos e alunas matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -11,5 +11,6 @@ router.register('matriculas', MatriculasViewSet, basename='Matriculas')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('explorer/', include(router.urls)),
-    path('explorer/aluno/<int:pk>/matriculas', ListaMatriculasAluno.as_view())
+    path('explorer/aluno/<int:pk>/matriculas', ListaMatriculasAluno.as_view()),
+    path('explorer/curso/<int:pk>/alunos', ListaAlunosMatriculados.as_view()),
 ]
